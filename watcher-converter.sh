3eb8ffe1b00844af78ea906efed067d10858d2c2
Cillian Scott <73595592+CillySu@users.noreply.github.com>
2023-04-06 09:50:53 +0100

    Update watcher-converter.sh

--- watcher-converter.sh
+++ watcher-converter.sh
@@ -5,6 +5,7 @@ WATCH_DIR="/mnt/wd/moodle-dl" # The script watches for files which are modified 
 DEST_DIR="/mnt/wd/consume" # The script places files here after either converting Office files from the WATCH_DIR to PDF, or after a PDF populates WATCH_DIR
 RENAME_DEST_DIR="/mnt/wd/consume" # The directory where the script places files after renaming them
 PROCESSED_FILE="/mnt/wd/services/mdl-watcher/processed_files.txt"
+PY_NAMER="/mnt/wd/shell-scripts/pdf-namer.py" # Location of the python renamer script
 
 # Watch for new files in $WATCH_DIR
 inotifywait -m -r -q --format '%w%f' -e create,moved_to "$WATCH_DIR" |
@@ -35,7 +36,7 @@ done &
 while true; do
       inotifywait -q -m -e create --format '%f' "${RENAME_DEST_DIR}" | while read FILENAME; do
     if [[ "${FILENAME}" == *.pdf ]]; then
-      python3 /mnt/wd/shell-scripts/pdf-namer.py "${RENAME_DEST_DIR}/${FILENAME}"
+      python3 "${PY_RENAMER}" "${RENAME_DEST_DIR}/${FILENAME}"
     fi
   done
 done
